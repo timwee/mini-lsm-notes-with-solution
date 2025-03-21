d4b2f62b7d8adb0021fe88fe55ded9b906157705
Tim Wee <tim.shawn@gmail.com>
2025-03-21 16:58:36 -0400

    1.3

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -19,10 +19,31 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::BlockIterator;
 
+pub(crate) const SIZEOF_U16: usize = std::mem::size_of::<u16>();
+
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
+/// ----------------------------------------------------------------------------------------------------
+/// |             Data Section             |              Offset Section             |      Extra      |
+/// ----------------------------------------------------------------------------------------------------
+/// | Entry #1 | Entry #2 | ... | Entry #N | Offset #1 | Offset #2 | ... | Offset #N | num_of_elements |
+/// ----------------------------------------------------------------------------------------------------
+///
+/// Each entry is a key-value pair.
+/// -----------------------------------------------------------------------
+/// |                           Entry #1                            | ... |
+/// -----------------------------------------------------------------------
+/// | key_len (2B) | key (keylen) | value_len (2B) | value (varlen) | ... |
+/// -----------------------------------------------------------------------
+///
+/// At the end of the block, we store the number of elements in the block.
+/// -------------------------------
+/// |offset|offset|num_of_elements|
+/// -------------------------------
+/// |   0  |  12  |       2       |
+/// -------------------------------
 pub struct Block {
     pub(crate) data: Vec<u8>,
     pub(crate) offsets: Vec<u16>,
@@ -32,11 +53,37 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the course
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let data_len = self.data.len();
+        let offsets_len = self.offsets.len();
+        let total_size = data_len + (offsets_len * 2) + 2; // data + offsets + offset count
+        let mut buf = Vec::with_capacity(total_size);
+
+        // Copy the data
+        buf.extend_from_slice(&self.data);
+
+        // Add offsets
+        for offset in &self.offsets {
+            buf.put_u16(*offset);
+        }
+
+        // Add number of elements at the end of the block
+        buf.put_u16(offsets_len as u16);
+        buf.into()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        // get number of elements in the block
+        let entry_offsets_len = (&data[data.len() - SIZEOF_U16..]).get_u16() as usize;
+        let data_end = data.len() - SIZEOF_U16 - entry_offsets_len * SIZEOF_U16;
+        let offsets_raw = &data[data_end..data.len() - SIZEOF_U16];
+        // get offset array
+        let offsets = offsets_raw
+            .chunks(SIZEOF_U16)
+            .map(|mut x| x.get_u16())
+            .collect();
+        // retrieve data
+        let data = data[0..data_end].to_vec();
+        Self { data, offsets }
     }
 }
